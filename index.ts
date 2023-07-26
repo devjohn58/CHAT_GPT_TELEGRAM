61764c0ddb8e7a3d4e028bde56377b77ee6c55de
devjohn58 <devjohn58@gmail.com>
2023-07-26 16:42:00 +0700

    fix 16.41

--- index.ts
+++ index.ts
@@ -8,7 +8,20 @@ if (!process.env.TELEGRAM_BOT_TOKEN) {
 
 export const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 import "./commands";
-
+try {
+    bot.api.setMyCommands([
+      {
+        command: "start",
+        description: "Starting with our bot.",
+      },
+      {
+        command: "chat",
+        description: "Use command /chat { your content } to chat with me.",
+      },
+    ]);
+  } catch (error) {
+    console.error("[Error] Could not set bot commands.", error);
+  }
 bot.start().catch((error) => {
   console.error("[Error] Could not start bot.", error);
 });
